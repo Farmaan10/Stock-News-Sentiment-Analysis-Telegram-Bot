4450a35d168e925f2dbe3a9b162bca91174b7584
Syed Farmaan <68768741+Farmaan10@users.noreply.github.com>
2022-04-11 02:53:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ from flask import Flask
 from flask import request
 from flask import Response
 
-#from tokens import telegram_token
-#from tickersList import tickersList
+from tokens import telegram_token
+from tickersList import tickersList
 
 import nltk
 nltk.download('stopwords')
@@ -43,14 +43,14 @@ def parse_telegramMessage(message):
     return chat_id, symbol
 
 def send_message(chat_id, text='Temp text'):
-    url = f'https://api.telegram.org/bot{5151075958:AAE2HQgFExKfpatTd9orFRzJNaCpi29Xo-M}/sendMessage'
+    url = f'https://api.telegram.org/bot5151075958:AAE2HQgFExKfpatTd9orFRzJNaCpi29Xo-M/sendMessage'
     payload = {'chat_id': chat_id, 'text': text} # Required parameters for sending a message is chat_id and text
 
     r = requests.post(url, json=payload)
     return r
 
 def send_photo(chat_id, sentiment):
-    url = f'https://api.telegram.org/bot{5151075958:AAE2HQgFExKfpatTd9orFRzJNaCpi29Xo-M}/sendPhoto'
+    url = f'https://api.telegram.org/bot5151075958:AAE2HQgFExKfpatTd9orFRzJNaCpi29Xo-M/sendPhoto'
     payload = {'chat_id': chat_id}
     files = {'photo': open(sentiment,'rb')}
     r = requests.post(url, payload,files=files)
@@ -91,10 +91,9 @@ def index():
         return '<h1>Stock News Sentiment Analysis Bot</h1>'
 
 def main():
-    print("Hi")
-    # https://api.telegram.org/bot1857156700:AAG9XIp3puMK0FggJM4PDfQzUQ_97xLtwRQ/getMe
-    # https://api.telegram.org/bot1857156700:AAG9XIp3puMK0FggJM4PDfQzUQ_97xLtwRQ/sendMessage?chat_id=1150227166&text=Hello Keshav
-    # https://api.telegram.org/bot1857156700:AAG9XIp3puMK0FggJM4PDfQzUQ_97xLtwRQ/setWebhook?url=https://c71ae6286836.ngrok.io
+    https://api.telegram.org/bot5151075958:AAE2HQgFExKfpatTd9orFRzJNaCpi29Xo-M/getMe
+    # https://api.telegram.org/bot5151075958:AAE2HQgFExKfpatTd9orFRzJNaCpi29Xo-M/sendMessage?chat_id=782938461&text=Hello Farmaan
+    # https://api.telegram.org/bot5151075958:AAE2HQgFExKfpatTd9orFRzJNaCpi29Xo-M/setWebhook?url=https://c71ae6286836.ngrok.io
 
 def sentimentAnalysis(ticker):
     finviz_url = 'https://finviz.com/quote.ashx?t='
