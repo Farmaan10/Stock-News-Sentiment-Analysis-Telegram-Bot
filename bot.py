4ec41c72582b055010c9079e3c52641ca229faed
Syed Farmaan <68768741+Farmaan10@users.noreply.github.com>
2022-04-13 20:15:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -107,15 +107,13 @@ def index():
         vectorizer = pickle.load(open('lem_tf_vectorizer.pkl', 'rb'))
         model = pickle.load(open('lem_tf_lr_model_pkl', 'rb'))
         news_vector = vectorizer.transform(ppd_data['title'])
-        time.sleep(1)
         ppd_data['ML model Prediction'] = model.predict(news_vector)
-        time.sleep(1)
         total_len = len(ppd_data['ML model Prediction'])
         pos_len = len(ppd_data[ppd_data['ML model Prediction']==1])
         neg_len = len(ppd_data[ppd_data['ML model Prediction']==-1])
         neu_len = len(ppd_data[ppd_data['ML model Prediction']==0])
-        time.sleep(2)
-        #send_message(chat_id, "The total number of news article headlines scanned were " + total_len + ", out of these there were:\n" + pos_len + "are Positive News\n" + neg_len + " are Negative News, and\n" + neu_len + " are Neutral News.")
+        time.sleep(1)
+        send_message(chat_id, "The total number of news article headlines scanned were " + str(total_len) + ", out of these there were:\n" + str(pos_len) + "are Positive News\n" + str(neg_len) + " are Negative News, and\n" + str(neu_len) + " are Neutral News.")
 
         #total_news = hybrid_model(chat_id, ppd_data)
         
